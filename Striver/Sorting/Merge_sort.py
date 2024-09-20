9e0d0eda4c97350750551ff722fd483c8de72980
OnShubham <shubhamkharche3005@gmail.com>
2024-09-20 16:00:36 +0530

    git add .

--- Striver/Sorting/Merge_sort.py
+++ Striver/Sorting/Merge_sort.py
@@ -3,44 +3,39 @@ def Fun(arr, n):
         # Find the middle point and divide the array into two halves
         mid = n // 2
         left_half = arr[:mid]
-        righ_half = arr[mid:]
+        right_half = arr[mid:]
 
         # Recursively call merge_sort on both halves
-        Fun(left_half)
-        Fun(righ_half)
+        Fun(left_half, len(left_half))
+        Fun(right_half, len(right_half))
 
         # Merging the sorted halves
-
         i = j = k = 0
 
         # Copy data to temporary arrays left_half[] and right_half[]
-
-        while i < len(left_half) and j < len(righ_half):
-            if left_half[i] < righ_half[j]:
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
                 arr[k] = left_half[i]
                 i += 1
             else:
-                arr[k] = righ_half[j]
+                arr[k] = right_half[j]
                 j += 1
             k += 1
 
         # Checking if any elements were left in the left_half
         while i < len(left_half):
-
             arr[k] = left_half[i]
             i += 1
             k += 1
 
         # Checking if any elements were left in the right_half
-
-        while j < len(righ_half):
-            arr[k] = righ_half[j]
+        while j < len(right_half):
+            arr[k] = right_half[j]
             j += 1
             k += 1
-    return arr
 
 
 arr = [3, 2, 1, 5, 6, 3, 2, 3, 45, 42, 1, 1, 3]
 n = len(arr)
-print(Fun(arr, n))
+Fun(arr, n)
 print(arr)
