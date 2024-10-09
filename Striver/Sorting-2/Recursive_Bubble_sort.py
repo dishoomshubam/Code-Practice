6a5cc05d5d940a594109ff116bfe1b3cd841c02a
OnShubham <shubhamkharche3005@gmail.com>
2024-10-09 16:19:04 +0530

    merge

---
+++ Striver/Sorting-2/Recursive_Bubble_sort.py
@@ -0,0 +1,37 @@
+def bubble_sort_recursive(arr, n):
+
+    if n == 1:
+        return
+
+    for i in range(n - 1):
+
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    bubble_sort_recursive(arr, n - 1)
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+n = len(arr)
+Fun = bubble_sort_recursive(arr, n)
+print(arr)
+
+
+def bubble_sort(arr, n):
+
+    if n == 1:
+        return
+
+    for i in range(n - 1):
+
+        if arr[i] > arr[i + 1]:
+
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    bubble_sort(arr, n - 1)
+
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+n = len(arr)
+bubble_sort(arr, n)
+print(arr)
